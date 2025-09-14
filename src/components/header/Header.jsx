ffe9875b753f403c3292582b5a2bc17cb6af1915
aryansh-2003 <dixitsanjeev1975@gmail.com>
2025-09-14 12:51:03 +0530

    error resolved

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -131,7 +131,7 @@ function Header() {
   return (
     <div className="md:fixed flex flex-row md:flex-col items-center bottom-0 w-full fixed   md:left-0 md:top-0 md:h-full md:w-60 bg-white border-r border-gray-200 z-200">
       {/* Logo Section */}
-      <div className="p-6 border-b border-gray-100  md:visible">
+      <div className="p-6 border-b border-gray-100 hidden md:block  md:visible">
         <Link to="/" className="block">
           <Logo width="120px"/>
         </Link>
